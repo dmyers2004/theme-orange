f7d889043ab24f452aa67a189f0eb2f284199e61
Don Myers <donmyers@quadratec.com>
2015-10-30 22:27:52 -0400

    updates

--- public/themes/orange/assets/js/orange.js
+++ public/themes/orange/assets/js/orange.js
@@ -1,6 +1,7 @@
 var mvc = (mvc) || {};
+var o = (o) || {};
 
-var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h3>Processing...</h3></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+var pleaseWaitDiv = $('<div class="modal fade bs-example-modal-sm" id="myPleaseWait" tabindex="-1"role="dialog" aria-hidden="true" data-backdrop="static"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 class="modal-title"><span class="glyphicon glyphicon-time"></span> Processing</h4></div><div class="modal-body"><div class="progress"><div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only"></span></div></div></div></div></div></div>');
 
 /*
 hide / show modals
